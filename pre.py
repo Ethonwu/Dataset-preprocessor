ad2a765081b25ee230d7df3d5ed1788e79c368a0
Ethonwu <a5351998@gmail.com>
2017-09-22 16:40:15 +0800

    Fix bug

--- pre.py
+++ pre.py
@@ -6,18 +6,33 @@ import os
 from optparse import OptionParser
 def AnalyseInputFile(inputFile,pro_symbol):
     Convert_list = dict()
-    i = 1
+    j = 1
     with open(inputFile,'r') as f:
         for line in f.readlines():
             s = line.strip('\n')
-            l = s.replace(pro_symbol,",")
-            cp_l = l
-            for w in l:
-                if "," is not w:
-                    if w not in Convert_list:
-                        Convert_list[w] = i
-                        i = i + 1
-                    cp_l = cp_l.replace(w,str(Convert_list[w]))            
+            cp_l = ""
+            symbol_index = []
+            for symbol in pro_symbol:
+                for i in range(0,len(s)):
+                    if s[i] == symbol:
+                        symbol_index.append(i)
+            s_tmp = ""
+            s_tmp = s[0:symbol_index[0]]
+            if s_tmp not in Convert_list:
+                Convert_list[s_tmp] = j
+                j = j + 1
+                cp_l = str(Convert_list[s_tmp])
+            else:
+                cp_l = str(Convert_list[s_tmp])
+            for i in range(1,len(symbol_index)):
+                s_tmp = ""
+                s_tmp = s[symbol_index[i-1]+1:symbol_index[i]]
+                if s_tmp not in Convert_list:
+                    Convert_list[s_tmp] = j
+                    j = j + 1
+                    cp_l = cp_l + "," + str(Convert_list[s_tmp])
+                else:
+                    cp_l = cp_l + "," + str(Convert_list[s_tmp])
             OutputFile(cp_l)
     return Convert_list
 def OutputFile(Data):
@@ -25,41 +40,14 @@ def OutputFile(Data):
     f.write(Data)
     f.write('\n')
     f.close()
-def CheckBetweenValue(Datasetfile):
-    symbol_list = dict()
-    check = 0
-    with open(Datasetfile,'r') as f:
-        for line in f.readlines():
-            if ' ' in line:
-                if check == 0:
-                    print "====================="
-                    print "File have space value"
-                    print "====================="
-                    check = 1
-                #finish_string = str(pre_string).split()
-                #value = 1
-                s = ' '
-                symbol_list[s] = 1
-            for pun in string.punctuation:
-                if pun in line:
-                    if check == 0:
-                        print "======================"
-                        print "File have symbol value"
-                        print "======================"
-                        check = 1
-                    symbol_list[pun] = 1
-    if len(symbol_list) > 1:
-        print "File input error please check input file!!"
-        sys.exit()
-    symbol = symbol_list.keys()[0]
-    return symbol
 if __name__ =="__main__":
     get_Table = dict()
     global filename
     optparser = OptionParser("useage: %prog"+"-f <input dataset File>"+"-o <output covert dataset path>"+"-O <output convert dataset to HDFS>")
     optparser.add_option('-f', '--inputFile',dest='input',help='filename',default=None)
     optparser.add_option('-o','--output',dest='output',help='Output filename',default='Output.txt')
-    optparser.add_option('-D','--outputHDFS',dest='output',help='Output filename',default='Output.txt')
+    #optparser.add_option('-D','--outputHDFS',dest='output',help='Output filename',default='Output.txt')
+    optparser.add_option('-s','--getfileSymbol',dest='symbol',help="Enter your file's symbol",default=' ')
     (options, args) = optparser.parse_args()
     if options.input is None:
         print sys.usage
@@ -75,7 +63,7 @@ if __name__ =="__main__":
         print "Here already have file"
         sys.exit()
     print "Now do pre process from file.........."
-    get_symbol = CheckBetweenValue(inFile)
+    get_symbol = options.symbol
     get_Table = AnalyseInputFile(inFile,get_symbol)
     d = open(dictfile,'a+')
     print "Your dataset list table is:"
